c474c2f56ee51be44c69747981133370e97c1bb1
alex.korotkykh <ak@figo.io>
2017-09-06 14:28:49 +0200

    basic http server setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-export class Cli {}
+import * as http from 'http';
+import * as winston from 'winston';
+
+const logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)(),
+    ]
+});
+
+function requestHandler(request: http.IncomingMessage,
+                        response: http.ServerResponse): void {
+    logger.debug(request.url);
+}
+
+const server = http.createServer(requestHandler);
+const port = process.env.PORT;
+
+server.listen(port, (err: any) => {
+    if (err) {
+        return logger.error('something bad happened', err);
+    }
+
+    logger.info(`server is listening on ${port}`);
+});
