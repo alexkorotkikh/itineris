0acd5d4ba372d3293a5168901f8f8f81a9b34421
alex.korotkykh <ak@figo.io>
2017-09-15 17:27:12 +0200

    start server on config change

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
+import * as http from 'http';
+
 import * as Rx from 'rxjs';
+import * as winston from "winston";
 
 import { EndpointInfo } from "./endpoints";
-import * as winston from "winston";
 
 
 export class ServerManager {
@@ -11,9 +13,18 @@ export class ServerManager {
         this.logger = logger;
     }
 
-    updateEndpoints(changedEndpoint: EndpointInfo): Rx.Observable<string> {
+    updateEndpoints(endpoint: EndpointInfo): Rx.Observable<string> {
         return Rx.Observable.create((observer: Rx.Observer<string>) => {
-            this.logger.info(JSON.stringify(changedEndpoint));
+            this.logger.info(JSON.stringify(endpoint));
+
+            const server = http.createServer((req, res) => {
+                this.logger.debug(`${req.method} ${req.url}`);
+            });
+
+            endpoint.nodeInfos.forEach(nodeInfo => {
+                server.listen(nodeInfo.port);
+            });
+
             observer.next("OK");
         });
     }
