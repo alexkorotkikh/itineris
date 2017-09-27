eebb2b901c1ee8936604e7e564e1c35a1f009061
alex.korotkykh <ak@figo.io>
2017-09-27 14:01:38 +0200

    draft request forwarding

--- src/target-router.ts
+++ src/target-router.ts
@@ -1,14 +1,19 @@
 import * as http from 'http';
 import * as etcd from 'promise-etcd';
 import * as Rx from 'rxjs';
+import * as rq from 'request';
+import * as url from 'url';
 
 export class TargetRouter {
   private etc: etcd.EtcdObservable;
   private changeWaiter: etcd.ChangeWaiter;
+  private routes: Route[];
 
   constructor(etc: etcd.EtcdObservable) {
     this.etc = etc;
+    this.routes = [];
     this.changeWaiter = etc.createChangeWaiter('targets');
+    this.changeWaiter.subscribe(this.updateRoutes);
   }
 
   route(req: http.IncomingMessage, res: http.ServerResponse): void {
@@ -19,13 +24,35 @@ export class TargetRouter {
 
   private findTarget(req: http.IncomingMessage): Rx.Observable<Apply> {
     return Rx.Observable.create((observer: Rx.Observer<Apply>) => {
-      observer.next((req2, res) => {
-        res.statusCode = 500;
-        res.write('500 Internal Server Error');
-        res.end();
-      });
+      this.getRoute(req, observer) || this.internalError(observer);
     });
   }
+
+  private getRoute(req: http.IncomingMessage, observer: Rx.Observer<Apply>) {
+    const route = this.routes.find(route => route.isApplicable(req));
+    observer.next((request, response) => {
+      request
+        .pipe(rq({ qs: url.parse(request.url).query, uri: route.url.toString() }))
+        .pipe(response);
+    });
+  }
+
+  private internalError(observer: Rx.Observer<Apply>) {
+    observer.next((req, res) => {
+      res.statusCode = 500;
+      res.write('500 Internal Server Error');
+      res.end();
+    });
+  }
+
+  private updateRoutes() {
+  }
+}
+
+interface Route {
+  url: URL;
+
+  isApplicable(req: http.IncomingMessage): boolean;
 }
 
 interface Apply {
